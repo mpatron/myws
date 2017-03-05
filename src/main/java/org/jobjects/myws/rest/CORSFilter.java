55b5bc4bb896cf3583d87ddba42703ada02663ea
Mickael Patron <mickael_patron@hotmail.com>
2017-03-05 17:53:26 +0100

    append

--- src/main/java/org/jobjects/myws/rest/CORSFilter.java
+++ src/main/java/org/jobjects/myws/rest/CORSFilter.java
@@ -20,14 +20,12 @@ import javax.ws.rs.ext.Provider;
 @PreMatching
 public class CORSFilter implements ContainerResponseFilter {
   @Override
-  public void filter(final ContainerRequestContext requestContext,
-      final ContainerResponseContext cres) throws IOException {
+  public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext cres)
+      throws IOException {
     cres.getHeaders().add("Access-Control-Allow-Origin", "*");
-    cres.getHeaders().add("Access-Control-Allow-Headers",
-        "origin, content-type, accept, authorization");
+    cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
     cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
-    cres.getHeaders().add("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     cres.getHeaders().add("Access-Control-Max-Age", "1209600");/* 14 jours */
   }
 }
