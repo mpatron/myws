c3e2c97bca506ee1cd48c54e31cd4042843539a1
Mickael Patron <mickael_patron@hotmail.com>
2017-04-17 01:34:54 +0200

    modif

--- src/test/java/org/jobjects/myws/tools/log/JObjectsLogFormatter.java
+++ src/test/java/org/jobjects/myws/tools/log/JObjectsLogFormatter.java
@@ -24,15 +24,15 @@ import org.jboss.logmanager.Level;
  */
 public class JObjectsLogFormatter extends Formatter {
   private static final DateFormat format = new SimpleDateFormat("YYYY-MM-dd hh:mm:ss");
-  public static final String ANSI_RESET = "\u001B[0m";
-  public static final String ANSI_BLACK = "\u001B[30m";
-  public static final String ANSI_RED = "\u001B[31m";
-  public static final String ANSI_GREEN = "\u001B[32m";
-  public static final String ANSI_YELLOW = "\u001B[33m";
-  public static final String ANSI_BLUE = "\u001B[34m";
-  public static final String ANSI_PURPLE = "\u001B[35m";
-  public static final String ANSI_CYAN = "\u001B[36m";
-  public static final String ANSI_WHITE = "\u001B[37m";
+  public static final String ANSI_RESET = "\u001B[1;0m";
+  public static final String ANSI_BLACK = "\u001B[1;30m";
+  public static final String ANSI_RED = "\u001B[1;31m";
+  public static final String ANSI_GREEN = "\u001B[1;32m";
+  public static final String ANSI_YELLOW = "\u001B[1;33m";
+  public static final String ANSI_BLUE = "\u001B[1;34m";
+  public static final String ANSI_PURPLE = "\u001B[1;35m";
+  public static final String ANSI_CYAN = "\u001B[1;36m";
+  public static final String ANSI_WHITE = "\u001B[1;37m";
 
   /**
    * Attention, il faut rendre la methode appelable 1 fois.
